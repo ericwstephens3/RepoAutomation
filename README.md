1ea0a479d8c0d6f39b41eaa4b6d6ea3285ff1ccb
Eric Stephens <ericwstephens3@gmail.com>
2019-06-03 15:06:31 -0400

    Updated README and added .sh

---
+++ README.md
@@ -0,0 +1,15 @@
+# GitHub Repository Automation
+This bash script creates Github repositories through a single simple bash command. The command accepts two parameters. The first being the framework/language and the second being the name of the repository to create.
+
+# Installation
+- clone repository into desired location
+- edit .sh file to change the path's, add/remove language support, etc. Also replace all instances of UNAME with your Github username
+- create an ssh key for GitHub
+  - link: https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+- edit your .bashrc to permanently add the command to your terminal
+'''sh
+gedit ~./bashrc
+'''
+'''sh
+source /path/to/.sh
+'''
---
+++ my_commands.sh
@@ -0,0 +1,56 @@
+ 
+#!/bin/bash
+
+# accepts 2 parameters
+# $1 is the framework/language
+# $2 is the repository/folder name
+function create() {
+    cd
+    #replace the following if statement with your own 
+	if [ $1 = "android" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+    elif [ $1 = "flutter" ]; then
+	cd /ADD/CUSTOM/PATH
+	flutter create $2
+	cd $2
+    elif [ $1 = "webdev" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+    elif [ $1 = "c" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+    elif [ $1 = "c++" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+    elif [ $1 = "python" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+    elif [ $1 = "java" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+	elif [ $1 = "bash" ]; then
+	cd /ADD/CUSTOM/PATH
+	mkdir $2
+	cd $2
+     else
+	echo "unknown parameter"
+	exit
+    fi
+    curl -u 'UNAME' https://api.github.com/user/repos -d '{"name": "'$2'"}'
+    git init
+    git remote add origin git@github.com:UNAME/$2.git
+    touch README.md
+    git add .
+    git commit -m "Initial commit"
+    git push -u origin master
+    if [ $1 != "android" ]; then
+    	code .
+    fi
+}
